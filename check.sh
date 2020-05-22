a6d601f39178e07c09a22baf5f5210524f25dfc2
testervic <469858846@qq.com>
2020-05-22 17:58:34 +0800

    Fix shell too

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ cd /Users/vic/.jenkins/workspace/TestApi/report/report
 echo `pwd`
 echo "清空report"
 echo `rm -rf *`
-echo `jmeter -n -t /Users/vic/.jenkins/workspace/TestApi/jmx/Demo.jmx -j /Users/vic/.jenkins/workspace/TestApi/report/jmeter.log -l /Users/vic/.jenkins/workspace/TestApi/report/jtl/report_'${date_time}'.jtl -e -o /Users/vic/.jenkins/workspace/TestApi/report/report`
+echo `jmeter -n -t /Users/vic/.jenkins/workspace/TestApi/jmx/Demo.jmx -j /Users/vic/.jenkins/workspace/TestApi/report/jmeter.log -l /Users/vic/.jenkins/workspace/TestApi/report/jtl/report_"${date_time}".jtl -e -o /Users/vic/.jenkins/workspace/TestApi/report/report`
 jtl_report=`find /Users/vic/.jenkins/workspace/TestApi/report/jtl -type f -exec basename {} \;| sort -n|tail -n 1`
 echo "最新jtl:${jtl_report}"
 
